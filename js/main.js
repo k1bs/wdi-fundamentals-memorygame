bed9a17aa06a7d6a7cbbd6e75e571efe1701d9f1
Alex Kibler <alexkibler@me.com>
2017-08-25 10:37:42 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 console.log("Up and running!");
-var cards = ["queen", "king", "queen", "king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
-	message = cardsInPlay[0] === cardsInPlay[1] ? "You found a match!" : "Sorry, try again.";
-	alert(message);
-} else {
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  	} else {
+      console.log("Sorry, try again.");
+  	};
+};
+var flipCard = function(cardId) {
+	console.log("User flipped over a " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
 
-}
+flipCard(0);
+flipCard(1);
+
 // console.log("User flipped " + cardOne);
 // console.log("User flipped " + cardFour);
