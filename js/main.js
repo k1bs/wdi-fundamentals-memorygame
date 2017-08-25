d3976aa4528ef6ddf78b3abdb96f130cb421633d
Alex Kibler <alexkibler@me.com>
2017-08-25 09:44:32 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	message = cardsInPlay[0] === cardsInPlay[1] ? "You found a match!" : "Sorry, try again.";
+	alert(message);
+} else {
+
+}
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardFour);
